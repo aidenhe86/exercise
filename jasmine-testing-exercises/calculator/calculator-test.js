b2de681d7302483aac9012bcf8012b48e6f94901
Aiden <aidenhe86@gmail.com>
2021-12-10 21:13:18 -0500

    finish calculator

--- jasmine-testing-exercises/calculator/calculator-test.js
+++ jasmine-testing-exercises/calculator/calculator-test.js
@@ -1,11 +1,29 @@
 
-it('should calculate the monthly rate correctly', function () {
+it('should calculate the monthly payment correctly', function () {
   // ...
+  const values = {amount: 10000, years : 10, rate : 2};
+  expect(calculateMonthlyPayment(values)).toEqual(`92.01`);
+
 });
 
 
 it("should return a result with 2 decimal places", function() {
   // ..
+  const values = {amount: 10043, years:8, rate:5.8};
+  expect(calculateMonthlyPayment(values)).toEqual('131.00');
 });
+
+it("should output a NaN if values are not numbers",function(){
+  const values={amount: `test`, years:10 ,rate:2};
+  expect(calculateMonthlyPayment(values)).toEqual('NaN');
+  values.amount = 10000;
+  values.years = `test`;
+  expect(calculateMonthlyPayment(values)).toEqual('NaN');
+  values.years = 10;
+  values.rate = `test`;
+  expect(calculateMonthlyPayment(values)).toEqual('NaN');
+});
+
+
 
 /// etc
--- jasmine-testing-exercises/calculator/calculator.js
+++ jasmine-testing-exercises/calculator/calculator.js
@@ -21,20 +21,40 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const initial = {amount : 10000, years : 10, rate : 2}
+  const amount = document.querySelector(`#loan-amount`);
+  const years = document.querySelector(`#loan-years`);
+  const rate = document.querySelector(`#loan-rate`);
+  amount.value = initial.amount;
+  years.value = initial.years;
+  rate.value = initial.rate;
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const currentValue = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentValue));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const i = values.rate / 100 / 12;
+  const n = values.years * 12;
+  const P = values.amount;
+  return ((P * i) / ( 1 - Math.pow((1 + i), -n))).toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyPayment = document.querySelector(`#monthly-payment`);
+  if(monthly === 'NaN' || monthly === 'Infinity'){
+    monthlyPayment.innerText = `Not a Valid Input!`;
+  }
+  else{
+    monthlyPayment.innerText = `$${monthly}`;
+  }
 }
