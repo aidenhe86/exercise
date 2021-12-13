8e4e7698ece2171fc445b174d6a1d7f89c82cb8f
Aiden <aidenhe86@gmail.com>
2021-12-13 16:36:35 -0500

    complete let and const Exercise

---
+++ let-and-const-exercise.js
@@ -0,0 +1,17 @@
+//change var to const to make PI constance
+const PI = 3.14;
+PI = 42; 
+
+//Quiz 1
+//Var can redeclare and reassign values, but let can only reassign values.
+
+//Quiz 2 
+//Unlike var, const can only declare and assign once.
+
+//Quiz3
+//Var can reassign value but const cannot.
+
+//Quiz 4
+//Hoisting is when declare variables with var, these variables will declare as undefined first before any other code runs.
+
+ 
