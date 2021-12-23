c4e8aa38f876ffa9b6fb9c7b2b6e592a02351a35
Aiden <aidenhe86@gmail.com>
2021-12-22 19:42:03 -0500

    complete partial web works exercise

---
+++ how-web-work-exercise.js
@@ -0,0 +1,11 @@
+//Part 1
+//HTTP is Hyper Text Transfer Protocol that how people get and send data to servers.
+//URL is Universal Resource Locators that is an address for some internet resource.
+//DNS is Domain Name System that turn hostnames into IP Address.
+//Query String is extra information to add in the URL to search, in the format of ?.
+//Get - Request information from server without side effects.
+//Post - Request information from server with side effects.
+//HTTP request is a request from client to server through HTTP.
+//HTTP response is a response from server to client through HTTP.
+//HTTP Headers provides additional information about the request/response. Like Accept, Accept-Language.
+//Browser will turn the hostname into IP address using DNS, then make a request to that IP address. Server will then response back with html, and browser will make a DOM depends on the response html and request again for anythings that needs to be request to the server, and server will response back again for each item that has been request.
