f0adc163466279140d9f9f9384dad16b900201fb
Aiden <aidenhe86@gmail.com>
2022-01-19 16:38:18 -0500

    complete flask session exercise

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template,flash,redirect
+from flask import Flask, request, render_template,flash,redirect,session
 from surveys import satisfaction_survey as survey
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -8,19 +8,19 @@ app.config['SECRET_KEY'] = "flash"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
+res_key = "responses"
+
 
 @app.route("/")
 def survey_page():
     """ show survey page to start"""
-    # clear saved responses before begin
-    responses.clear()
     return render_template("survey.html", survey=survey)
 
 @app.route("/begin", methods=["POST"])
 def begin_survey():
     """ redirect question page from GET to POST"""
-
+    # clear saved responses before begin
+    session[res_key] = []
     return redirect("/questions/0")
 
 
@@ -28,8 +28,11 @@ def begin_survey():
 def questions_page(num):
     """show survey questions and save answers"""
 
+    #read responses cookies from flask
+    responses = session[res_key]
+
     # if user skip questions, redirect back to the questions
-    if num != len(responses):
+    if (num != len(responses)):
         flash("Invalid question! Please complete the survey in order")
         return redirect(f"/questions/{len(responses)}")
     
@@ -45,10 +48,13 @@ def questions_page(num):
 def answer():
     """ handle response and redirect to next question"""
     answer = request.form['answer']
+    responses = session[res_key]
     responses.append(answer)
+    session[res_key] = responses
 
     # redirect to answer page if all question have answered
     if len(responses) == len(survey.questions):
+        session["responses"] = responses
         return redirect("/complete")
 
     return redirect(f"/questions/{len(responses)}")
