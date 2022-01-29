c1f7427a8708d9243f570adf0440ae7db579f56a
Aiden <aidenhe86@gmail.com>
2022-01-29 17:49:26 -0500

    complete SQL QUerying exercise

---
+++ sql-querying/queries_playstore.sql
@@ -0,0 +1,16 @@
+-- Comments in SQL Start with dash-dash --
+-- 1. SELECT * FROM analytics WHERE id = 1880;
+-- 2. SELECT id,app_name FROM analytics WHERE last_updated = '2018-08-01';
+-- 3. SELECT category,COUNT(*) FROM analytics GROUP BY category;
+-- 4. SELECT * FROM analytics ORDER BY reviews desc LIMIT 5;
+-- 5. SELECT * FROM analytics WHERE rating >= 4.8 ORDER BY reviews desc LIMIT 1;
+-- 6. SELECT category,AVG(rating) FROM analytics GROUP BY category ORDER BY AVG desc;
+-- 7. SELECT app_name,price,rating FROM analytics WHERE rating < 3 ORDER BY price desc LIMIT 1;
+-- 8. SELECT * FROM analytics WHERE min_installs <= 50 and rating IS NOT NULL ORDER BY rating desc;
+-- 9. SELECT app_name FROM analytics WHERE rating < 3 and reviews >= 10000;
+-- 10. SELECT * FROM analytics WHERE price BETWEEN 0.10 AND 1.00 ORDER BY reviews desc LIMIT 10;
+-- 11. SELECT * FROM analytics WHERE last_updated =(SELECT MIN(last_updated) FROM analytics);
+-- 12. SELECT * FROM analytics WHERE price =(SELECT MAX(price) FROM analytics);
+-- 13. SELECT SUM(reviews) FROM analytics;
+-- 14. SELECT category FROm analytics GROUP BY category HAVING COUNT(*) > 300;
+-- 15. SELECT app_name,reviews,min_installs, min_installs/reviews AS proportion FROM analytics WHERE min_installs >=100000 ORDER BY proportion desc LIMIT 1;
