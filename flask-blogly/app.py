ffc200670d66be0df0e4d72c0048ba598d67e155
Aiden <aidenhe86@gmail.com>
2022-02-22 15:37:57 -0500

    complete blogly part 2

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 
@@ -24,14 +24,13 @@ def index():
 @app.route('/users')
 def homepage():
     """show a list of users"""
-    # users = User.query.order_by(User.last_name,User.first_name).all()
     users = User.query.all()
-    return render_template('index.html', users=users)
+    return render_template('user/index.html', users=users)
 
 @app.route('/users/new')
 def newform():
     """show a form to submit new user"""
-    return render_template('userform.html')
+    return render_template('user/userform.html')
 
 @app.route('/users/new',methods=["POST"])
 def adduser():
@@ -49,13 +48,13 @@ def adduser():
 def users_profire(user_id):
     """show user's profile"""
     user = User.query.get_or_404(user_id)
-    return render_template('show.html',user=user)
+    return render_template('user/show.html',user=user)
 
 @app.route('/users/<int:user_id>/edit')
 def edit_profile(user_id):
     """edit user's information"""
     user = User.query.get_or_404(user_id)
-    return render_template('profile.html',user=user)
+    return render_template('user/profile.html',user=user)
 
 @app.route('/users/<int:user_id>/edit', methods=["POST"])
 def update_profile(user_id):
@@ -80,3 +79,57 @@ def delete_profile(user_id):
 
     return redirect('/users')
 
+@app.route('/users/<int:user_id>/posts/new')
+def users_post(user_id):
+    """show create post page"""
+    user = User.query.get_or_404(user_id)
+    return render_template('post/newpost.html',user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new',methods=["POST"])
+def upload_post(user_id):
+    """upload user's post"""
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title = request.form["title"],
+                    content = request.form["content"],
+                    user_id = user.id
+    )
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f'/users/{user.id}')
+
+@app.route('/posts/<int:post_id>')
+def user_posts(post_id):
+    """Show user posts"""
+    post = Post.query.get_or_404(post_id)
+
+    return render_template("post/post.html",post=post)
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post(post_id):
+    """show post edit page"""
+    post = Post.query.get_or_404(post_id)
+
+    return render_template("post/edit_post.html",post = post)
+
+
+@app.route('/posts/<int:post_id>/edit',methods=["POST"])
+def update_post(post_id):
+    """update user's post"""
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form["title"]
+    post.content = request.form["content"]
+
+    db.session.add(post)
+    db.session.commit()
+    return redirect(f'/users/{post.user_id}')
+
+@app.route('/posts/<int:post_id>/delete',methods = ["POST"])
+def delete_post(post_id):
+    """delete user's post"""
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect('/users')
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -9,9 +10,21 @@ def connect_db(app):
 
 class User(db.Model):
     """ table User"""
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text)
+
+    posts = db.relationship('Post',backref='user')
+
+class Post(db.Model):
+    """ table Post"""
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime,nullable = False, default = datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable = False)
