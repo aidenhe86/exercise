37744fb11986c9c71e981d9d1c8de81105bfa58d
Aiden <aidenhe86@gmail.com>
2022-07-28 15:21:15 -0400

    complete recursion exercise

---
+++ dsa-recursion/recursion.js
@@ -0,0 +1,81 @@
+/** product: calculate the product of an array of numbers. */
+
+function product(nums, idx = 0) {
+  if (idx === nums.length) return 1;
+  return nums[idx] * product(nums, idx + 1);
+}
+
+/** longest: return the length of the longest word in an array of words. */
+
+function longest(words, idx = 0, length = 0) {
+  if (idx === words.length) return length;
+  length = Math.max(words[idx].length, length);
+  return longest(words, idx + 1, length);
+}
+
+/** everyOther: return a string with every other letter. */
+
+function everyOther(str, idx = 0, newStr = "") {
+  if (idx >= str.length) return newStr;
+  newStr += str[idx];
+  return everyOther(str, idx + 2, newStr);
+}
+
+/** isPalindrome: checks whether a string is a palindrome or not. */
+
+function isPalindrome(str, idx = 0) {
+  let leftIdx = idx;
+  let rightIdx = str.length - 1 - idx;
+  if (leftIdx >= rightIdx) return true;
+  if (str[leftIdx] !== str[rightIdx]) return false;
+  return isPalindrome(str, idx + 1);
+}
+
+/** findIndex: return the index of val in arr (or -1 if val is not present). */
+
+function findIndex(arr, val, idx = 0) {
+  if (arr.length === idx) return -1;
+  if (val === arr[idx]) return idx;
+  return findIndex(arr, val, idx + 1);
+}
+
+/** revString: return a copy of a string, but in reverse. */
+
+function revString(str, idx = 0, newStr = "") {
+  if (str.length === idx) return newStr;
+  newStr += str[str.length - 1 - idx];
+  return revString(str, idx + 1, newStr);
+}
+
+/** gatherStrings: given an object, return an array of all of the string values. */
+
+function gatherStrings(obj) {
+  let strArr = [];
+  for (let key in obj) {
+    if (typeof obj[key] === "string") strArr.push(obj[key]);
+    if (typeof obj[key] === "object") strArr.push(...gatherStrings(obj[key]));
+  }
+  return strArr;
+}
+
+/** binarySearch: given a sorted array of numbers, and a value,
+ * return the index of that value (or -1 if val is not present). */
+
+function binarySearch(arr, val, left = 0, right = arr.length - 1) {
+  if (left > right) return -1;
+  let mid = Math.floor((left + right) / 2);
+  if (arr[mid] === val) return mid;
+  else if (arr[mid] > val) return binarySearch(arr, val, left, mid - 1);
+  else return binarySearch(arr, val, left + 1, right);
+}
+
+module.exports = {
+  product,
+  longest,
+  everyOther,
+  isPalindrome,
+  findIndex,
+  revString,
+  gatherStrings,
+  binarySearch,
+};
